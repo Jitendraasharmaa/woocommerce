3c3bcc4aa499b4c52f7fd12709eb832ae84d636d
mtc <jitendra.sharma@rspinnovations.com>
2024-09-10 16:58:57 +0530

    Worked on recommended page

--- lib/pages/recommended_product_details/recommended_product_details.dart
+++ lib/pages/recommended_product_details/recommended_product_details.dart
@@ -18,28 +18,14 @@ class _RecommendedProductDetailsState extends State<RecommendedProductDetails> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(
-        children: [
-          Positioned(
-            left: 0,
-            right: 0,
-            child: Container(
-              width: double.maxFinite,
-              height: Dimensions.productSize,
-              decoration: const BoxDecoration(
-                color: AppColors.yellowColor,
-                image: DecorationImage(
-                  image: AssetImage("assets/images/img_1.png"),
-                  fit: BoxFit.cover,
-                ),
-              ),
-            ),
-          ),
-          Positioned(
-            top: 45,
-            left: 20,
-            right: 20,
-            child: Row(
+      body: CustomScrollView(
+        slivers: [
+          SliverAppBar(
+            expandedHeight: 300,
+            pinned: true,
+            backgroundColor: AppColors.yellowColor,
+            automaticallyImplyLeading: false,
+            title: Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
                 GestureDetector(
@@ -55,39 +41,107 @@ class _RecommendedProductDetailsState extends State<RecommendedProductDetails> {
                 ),
               ],
             ),
-          ),
-          Positioned(
-              left: 0,
-              right: 0,
-              bottom: 0,
-              top: Dimensions.productSize - 20,
+            bottom: PreferredSize(
+              preferredSize: const Size.fromHeight(50),
               child: Container(
-                padding: EdgeInsets.only(
-                    top: Dimensions.paddingTop20,
-                    left: Dimensions.paddingLeft20,
-                    right: Dimensions.paddingTop20),
+                width: double.maxFinite,
+                padding: const EdgeInsets.only(top: 10.0, bottom: 10),
+                alignment: Alignment.center,
                 decoration: const BoxDecoration(
                   borderRadius: BorderRadius.only(
-                    topLeft: Radius.circular(20),
-                    topRight: Radius.circular(20),
+                    topLeft: Radius.circular(10),
+                    topRight: Radius.circular(10),
+                  ),
+                  color: Colors.white,
+                ),
+                child: const BigText(
+                  title: "Chinese Salad",
+                ),
+              ),
+            ),
+            flexibleSpace: FlexibleSpaceBar(
+              background: Image.asset(
+                "assets/images/img_1.png",
+                width: double.maxFinite,
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+          SliverToBoxAdapter(
+            child: Container(
+              color: AppColors.whiteColor,
+              padding:
+                  EdgeInsets.symmetric(horizontal: Dimensions.paddingLeft20),
+              child: const ExpandableTextWidget(
+                  text:
+                      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."),
+            ),
+          )
+        ],
+      ),
+      bottomNavigationBar: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Container(
+            padding: EdgeInsets.symmetric(
+                horizontal: Dimensions.paddingLeft20,
+                vertical: Dimensions.paddingTop30),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const AppIconWidget(
+                  icon: Icons.remove,
+                  backgroundColor: AppColors.mainColor,
+                ),
+                SizedBox(width: Dimensions.sizeHeight20),
+                const BigText(title: "\$100.00/-"),
+                SizedBox(width: Dimensions.sizeHeight20),
+                const AppIconWidget(
+                  icon: Icons.add,
+                  backgroundColor: AppColors.mainColor,
+                )
+              ],
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Container(
+                    padding: EdgeInsets.all(Dimensions.paddingLeft20),
+                    decoration: BoxDecoration(
+                      color: AppColors.whiteColor,
+                      borderRadius:
+                          BorderRadius.circular(Dimensions.borderRadius20),
+                    ),
+                    child: const Icon(
+                      Icons.favorite,
+                      color: Colors.red,
+                      size: 50,
+                    )),
+                ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                    elevation: 0,
+                    backgroundColor: AppColors.mainColor,
+                    shape: RoundedRectangleBorder(
+                      borderRadius:
+                          BorderRadius.circular(Dimensions.borderRadius20),
+                    ),
+                    padding: EdgeInsets.symmetric(
+                      horizontal: Dimensions.paddingLeft30,
+                      vertical: Dimensions.paddingTop20 * 1.5,
+                    ),
+                  ),
+                  child: const BigText(
+                    title: "Add To Cart",
+                    color: AppColors.whiteColor,
                   ),
                 ),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    const SizedBox(height: 30),
-                    const BigText(
-                      title: "Biryani",
-                    ),
-                    SizedBox(height: Dimensions.sizeHeight10),
-                    const ExpandableTextWidget(
-                      text:
-                          "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
-                    ),
-                    SizedBox(height: Dimensions.sizeHeight20),
-                  ],
-                ),
-              ))
+              ],
+            ),
+          ),
         ],
       ),
     );
